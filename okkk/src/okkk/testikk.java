70927ee5052ac70db723558b2122ea1bdc26d889
admins <admins@192.168.0.12>
2017-05-01 01:21:21 +0530

    okkkkk second doneee

--- okkk/src/okkk/testikk.java
+++ okkk/src/okkk/testikk.java
@@ -4,6 +4,13 @@ public class testikk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 
 	}
 
